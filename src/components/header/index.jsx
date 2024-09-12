cd8136406ae8b4dc1bc1d2b3b74a43342e65d2b0
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-09-12 02:02:01 +0000

    header ui

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,6 +3,8 @@ import Logo from '../../assets/images/log.png'
 import CountryDropDown from '../countryDropDown';
 import { IoSearch } from "react-icons/io5";
 import { Button } from '@mui/material';
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoBagOutline } from "react-icons/io5";
 
 
 const Header = () => {
@@ -32,6 +34,18 @@ const Header = () => {
                                 <Button><IoSearch/></Button>
                             </div>
                             {/* Header ends */}
+
+                            <div className='part3 d-flex align-items-center ml-auto'>
+                                <Button className='circle mr-3'><FaRegCircleUser /></Button>
+                                <div className='ml-auto cartTab d-flex align-items-center'>
+                                    <span className='price'>$3.10</span>
+                                    <div className='postion-relative ml-2'>
+                                    <Button className='circle '><IoBagOutline /></Button>
+                                    <span className='count d-flex align-items-center justify-content-center'>1</span>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
